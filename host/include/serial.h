bd956dbc0f3a91836570ab517e2da72a1632bf0a
mundi207 <alsrb6008@gmail.com>
2026-05-14 15:55:14 +0900

    🚀Feat : 시리얼 포트 터미널 설정 코드 추가  Changes to be committed: 	modified:   include/serial.h 	modified:   src/log.c 	modified:   src/serial.c  Changes not staged for commit: 	modified:   ../firmware/platformio.ini

--- host/include/serial.h
+++ host/include/serial.h
@@ -1,24 +1,47 @@
 #ifndef SERIAL_H
 #define SERIAL_H
+#define _GNU_SOURCE
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 //연결될 MCP VID, PID 정의
 #define ESP32_VID 0x10c4
 #define ESP32_PID 0xea60
+
 #define LEONARDO_VID 0x3343
 #define LEONARDO_PID 0x803a
 
+//리눅스 상의 USB 정보 경로
 #define VID_PATH "/sys/bus/usb/devices/*/idVendor"
 #define PID_PATH "/sys/bus/usb/devices/*/idProduct"
 #define USB_PATH "/sys/bus/usb/devices/"
 
-void ping();
+//시리얼 포트 관련 전역변수
+extern char* leonardoPortName;
+extern int leonardoPortFd;
+extern struct termios old_ter;
+extern struct termios new_ter;
+
+//시리얼 포트 상태
+typedef enum
+{
+    INIT_OK = 0,
+    SERIAL_PORT_OPEN_FAILED = -1,
+    SERIAL_PORT_OPEN_FAILED = -2,
+    TERMINULL_LOAD_FAILED = -3,
+    TERMINULL_SAVE_FAILED = -4
+}SerialPortState;
+
+extern SerialPortState serialportstate;
 
 char* getserialport(unsigned int pid, unsigned int vid);
 
+int init();
+void ping();
 void senddata();
 void recvdata();
 
--- host/src/serial.c
+++ host/src/serial.c
@@ -1,6 +1,7 @@
 #include "serial.h"
 #include "command.h"
 #include "log.h"
+#include "powercmd.h"
 
 char* getserialport(unsigned int pid, unsigned int vid)
 {
@@ -10,30 +11,92 @@ char* getserialport(unsigned int pid, unsigned int vid)
     char* result_pid;
     char* result_vid;
 
-    sprintf(cmd, "grep -r \"%x\" %s", pid, PID_PATH);
-    buf = send_linux_command(cmd);
-
-    result_pid = strstr(buf, USB_PATH) + strlen(USB_PATH);
-    buf = result_pid;
-    result_pid[strcspn(buf, "/")] = '\0';
-
-    sprintf(cmd, "grep -r \"%x\" %s", vid, VID_PATH);
-    buf = send_linux_command(cmd);
-
-    result_vid = strstr(buf, USB_PATH) + strlen(USB_PATH);
-    buf = result_vid;
-    result_vid[strcspn(buf, "/")] = '\0';
-
-    // PID와 VID가 같은 경로에 있으면 포트가 유효함.
-    if(strcmp(result_pid, result_vid) == 0)
+    if(leonardoPortName == NULL)
     {
-        sprintf(cmd, "ls %s%s | grep tty", USB_PATH, result_pid);
+        sprintf(cmd, "grep -r \"%x\" %s", pid, PID_PATH);
         buf = send_linux_command(cmd);
 
-        char log[256];
+        result_pid = strstr(buf, USB_PATH) + strlen(USB_PATH);
+        buf = result_pid;
+        result_pid[strcspn(buf, "/")] = '\0';
 
-        sprintf(log, "연결된 포트 -> %s", buf);
-        prt_logconsole(log);
+        sprintf(cmd, "grep -r \"%x\" %s", vid, VID_PATH);
+        buf = send_linux_command(cmd);
+
+        result_vid = strstr(buf, USB_PATH) + strlen(USB_PATH);
+        buf = result_vid;
+        result_vid[strcspn(buf, "/")] = '\0';
+
+        // PID와 VID가 같은 경로에 있으면 포트가 유효함.
+        if(strcmp(result_pid, result_vid) == 0)
+        {
+            sprintf(cmd, "ls %s%s/* | grep tty", USB_PATH, result_pid);
+            buf = send_linux_command(cmd);
+
+            char log[256];
+
+            sprintf(log, "연결된 포트 -> %s", buf);
+            prt_logconsole(log);
+        }
+        return buf;
     }
-    return buf;
+    return leonardoPortName;
+}
+
+int init()
+{
+    leonardoPortName = getserialport(LEONARDO_PID, LEONARDO_VID);
+
+    if(leonardoPortName == NULL)
+    {
+        prt_logconsole("시리얼 포트 불러오기 실패..");
+        serialportstate = SERIAL_PORT_OPEN_FAILED;
+        return serialportstate;
+    }
+
+    leonardoPortFd = open(leonardoPortName, O_RDWR | O_NOCTTY | O_NONBLOCK);
+
+    if(leonardoPortFd < 1)
+    {
+        prt_logconsole("포트 열기 실패..");
+        serialportstate = SERIAL_PORT_OPEN_FAILED;
+        return serialportstate;
+    }
+
+    if(tcgetattr(leonardoPortFd, &old_ter) != 0)
+    {
+        prt_logconsole("시리얼 포트 터미널 로드 실패");
+        serialportstate = TERMINULL_LOAD_FAILED;
+        return serialportstate;
+    }
+    else
+    {
+        new_ter = old_ter;
+
+        new_ter.c_cflag &= ~(PARENB | CSTOPB | CRTSCTS);
+        new_ter.c_cflag |= (CS8 | CREAD | CLOCAL);
+
+        new_ter.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHONL | ISIG);
+
+        new_ter.c_iflag &= ~(IXON | IXOFF | IXANY);
+        new_ter.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL);
+
+        new_ter.c_oflag &= ~(OPOST | ONLCR);
+        new_ter.c_cc[VTIME] = 15;
+        new_ter.c_cc[VMIN] = 1;
+
+        cfsetispeed(&new_ter, B115200);
+
+        if(tcsetattr(leonardoPortFd, TCSANOW, &new_ter) != 0)
+        {
+            prt_logconsole("시리얼 포트 터미널 설정 실패");
+            serialportstate = TERMINULL_SAVE_FAILED;
+            return serialportstate;
+        }
+    }
+}
+
+void ping()
+{
+    
 }
